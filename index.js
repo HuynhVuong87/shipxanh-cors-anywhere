872b76bd4cada99a6592d6902a26f8b633f26ad4
HuynhVuong87 <hv198197@gmail.com>
2021-11-04 17:59:32 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen("8085", "0.0.0.0", function () {
+    console.log("Running CORS Anywhere on " + "0.0.0.0" + ":" + "8080");
+  });
